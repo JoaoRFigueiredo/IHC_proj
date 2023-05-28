e20835dc41e5b4c32219c5eab0ffc9d2163e4874
FranciscoCardita <franciscocardita@ua.pt>
2023-05-28 19:21:53 +0100

    Update favorites navbar and cards

--- ssd_frontend/lib/Favoritos.dart
+++ ssd_frontend/lib/Favoritos.dart
@@ -1,211 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:ssd_frontend/Interesses.dart';
-import 'package:ssd_frontend/AveiroInteressesPage.dart';
 import 'package:ssd_frontend/AboutUS.dart';
-import 'package:ssd_frontend/registo_empresas/registo.dart';
-import 'package:ssd_frontend/registo_empresas/signUp_pessoa.dart';
-import 'features_empresa/features_empresa.dart';
 import 'login/login_turista.dart';
-
-class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
-  const CustomAppBar({Key? key}) : super(key: key);
-
-  @override
-  Size get preferredSize => const Size.fromHeight(kToolbarHeight + 50);
-  //Size get preferredSize => MediaQuery.of(context).size;
-  @override
-  Widget build(BuildContext context) {
-    //favorites.add(1);
-    return Container(
-
-      margin: const EdgeInsets.all(20),
-      padding: const EdgeInsets.all(20),
-      // padding: const EdgeInsets.fromLTRB(10, 20, 20, 20), // ajuste aqui
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(46),
-        boxShadow: [
-          BoxShadow(
-            offset: const Offset(0, -2),
-            blurRadius: 40,
-            color: Colors.black.withOpacity(0.16),
-          ),
-        ],
-      ),
-
-
-      child: Row(
-        children: [
-
-          Container(
-            width: 40,
-            alignment: Alignment.centerLeft,
-            child: PopupMenuButton<String>(
-
-              onSelected: (value) {
-                switch (value) {
-                  case 'Favoritos':
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => Favoritos()),
-                    );
-                    break;
-                  case 'AboutUS':
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => const AboutUS()),
-                    );
-                    break;
-                  case 'interesses':
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => const Interesses()),
-                    );
-                    break;
-                  case 'Login':
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => const LoginTurista()),
-                    );
-                    break;
-                  case 'Registro':
-                  // Adicione a navegação para a página de registro aqui
-                    break;
-                }
-              },
-
-              itemBuilder: (BuildContext context) => <PopupMenuEntry<String>>[
-                const PopupMenuItem<String>(
-                  value: 'favoritos',
-                  child: Text('Favoritos'),
-                ),
-                const PopupMenuItem<String>(
-                  value: 'interesses',
-                  child: Text('Interesses'),
-                ),
-                const PopupMenuItem<String>(
-                  value: 'Login',
-                  child: Text('Login'),
-                ),
-                const PopupMenuItem<String>(
-                  value: 'Registro',
-                  child: Text('Criar Conta'),
-                ),
-                const PopupMenuItem<String>(
-                  value: 'AboutUS',
-                  child: Text('LusiTravel'),
-                ),
-              ],
-              child: const Icon(Icons.menu), // ícone do menu
-
-            ),
-          ),
-          GestureDetector(
-            onTap: () {
-              Navigator.popUntil(context, (route) => route.isFirst);
-            },
-            child: Padding(
-              padding: const EdgeInsets.only(left: 0),
-              child: Image.asset(
-                "assets/icons/icon_app.png",
-                height: 50,
-                width: 40,
-                alignment: Alignment.topLeft,
-              ),
-            ),
-          ),
-
-
-
-
-
-          const SizedBox(width: 5),
-          Expanded(
-            child: Container(
-              margin: const EdgeInsets.only(left: 10),
-              padding: const EdgeInsets.symmetric(horizontal: 16),
-              decoration: BoxDecoration(
-                color: Colors.grey.withOpacity(0.2),
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: const TextField(
-                decoration: InputDecoration(
-                  hintText: "Pesquisar",
-                  border: InputBorder.none,
-                  suffixIcon: Icon(Icons.search),
-                ),
-                style: TextStyle(fontSize: 18),
-              ),
-            ),
-          ),
-          const SizedBox(
-            width: 5,
-          ),
-
-
-          ElevatedButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => Favoritos(),
-                ),
-              );
-            },
-            child: const Text(
-              "Favoritos",
-              style: TextStyle(
-                fontSize: 18,
-              ),
-            ),
-          ),
-          const SizedBox(width: 10), // Adicionado aqui
-
-
-          ElevatedButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => const Interesses(),
-                ),
-              );
-            },
-            child: const Text(
-              "Interesses",
-              style: TextStyle(
-                fontSize: 18,
-              ),
-            ),
-          ),
-
-          const SizedBox(
-            width: 5,
-          ),
-          const SizedBox(
-            width: 5,
-          ),
-          ElevatedButton(
-              onPressed: () {
-                Navigator.push(context, MaterialPageRoute(
-                    builder: (context) => const LoginTurista())
-                );
-              },
-              child: const Text(
-                "Login",
-                style: TextStyle(
-                  fontSize: 18,
-                ),
-              )),
-          const SizedBox(
-            width: 5,
-          ),
-
-        ],
-      ),
-    );
-  }
-}
+import 'appbar_secondary.dart';
 
 class Restaurant {
   final String name;
@@ -249,7 +46,7 @@ class FavoritosState extends State<Favoritos> {
     @override
   void initState() {
     super.initState();
-    favorites.add(1); // Add the value 1 to the favorites set
+    favorites.add(1);
   }
 
   bool isFavorite = false;
@@ -257,7 +54,7 @@ class FavoritosState extends State<Favoritos> {
   Widget build(BuildContext context) {
     //favorites.add(1);
     return Scaffold(
-      appBar: const CustomAppBar(),
+      appBar: CustomAppBar_2(favorites: favorites,),
       body: SingleChildScrollView(
         child: SizedBox(
           height: MediaQuery.of(context).size.height,
@@ -267,9 +64,11 @@ class FavoritosState extends State<Favoritos> {
                 child: Padding(
                     padding: const EdgeInsets.all(16.0),
                     child: GridView.count(
-                      crossAxisCount: 2,
+                      crossAxisCount: 4,
                       mainAxisSpacing: 16,
                       crossAxisSpacing: 16,
+                      childAspectRatio: 1.75,
+                      
                       children: restaurants.map((restaurant) {
                         //bool isFavorite = true;
                         //favorites.add(restaurant.id);
@@ -333,10 +132,8 @@ class FavoritosState extends State<Favoritos> {
                               crossAxisAlignment: CrossAxisAlignment.start,
                               children: [
                                 Expanded(
-                                  child: LimitedBox(
-                                    maxHeight:
-                                        MediaQuery.of(context).size.height *
-                                            0.4,
+                                  child: Container(
+                                    width: double.infinity,
                                     child: Image.asset(
                                       restaurant.image,
                                       fit: BoxFit.cover,
